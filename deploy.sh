ba4742ceccf8d7215ebc05400f658ed8ce32ec23
root <root@e2e-111-194.ssdcloudindia.net>
2025-11-15 17:14:09 +0530

    configured acc to ubuntu server

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-cd "/root/FinsageMTMBackend" || exit
-/usr/local/bin/pm2 delete "Finsage mtm Backend"
-/usr/local/bin/pm2 start main.py --name "Finsage mtm Backend" --interpreter /root/venv/bin/python3.10 --no-autorestart
+# Modified lines in deploy.sh
+pm2 delete "Finsage mtm Backend"
+pm2 start main.py --name "Finsage mtm Backend" --interpreter /root/FinsageMTMBackend/venv/bin/python3.10 --no-autorestart
